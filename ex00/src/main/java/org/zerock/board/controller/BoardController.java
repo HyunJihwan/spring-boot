e78958934039354e333bc09a28e5177632dc4827
HyunJihwan <lsi6930@gmail.com>
2023-09-13 15:07:34 +0900

    dsadsad

---
+++ ex00/src/main/java/org/zerock/board/controller/BoardController.java
@@ -0,0 +1,17 @@
+package org.zerock.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BoardController {
+	
+	@RequestMapping(value = "/board/list.do", method = RequestMethod.GET)
+	public String list() {
+		return "/board/list";
+	}
+	
+	
+}
